a35b278f48a2fef9ac3680a8e89b5a5ab29c1a56
Adinath Shelke <shelkeadinath3@gmail.com>
2022-12-25 09:38:45 +0530

    Created an endpoint for authenticating user

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,12 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "InoteBookBuiltByAdi"
+const fetchuser = require("../middleware/fetchuser")
 
-//Creating a user using : POST "api/auth/createuser". No login required
+//ROUTE 1: Creating a user using : POST "api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -27,16 +31,85 @@ router.post(
           .status(400)
           .json({ error: "Sorry, a user with this email already exists" });
       }
+
+      // creating a salt to add with the hash password
+      const salt =await bcrypt.genSalt(10)
+      // creating a hash of the password
+      const secPass=await bcrypt.hash(req.body.password,salt) 
+      //Creating a user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      // creating a signature token to use for further authentication when the user is logged in
+      const authtoken=jwt.sign(data, JWT_SECRET)
+      res.json({authtoken});
+
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Oops! Something went wrong")
+      res.status(500).send("Internal Server Error")
     }
   }
 );
+
+//ROUTE 2: Authenticate user using POST "api/auth/login" no login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password","Password cannot be blank").exists(),
+  ],async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password}=req.body
+    try {
+      let user = await User.findOne({email})
+      if(!user)
+      {
+        return res.status(400).json({error:"Invalid Credentials, try again"})
+      }
+      const cmpPass=await bcrypt.compare(password,user.password)
+      if(!cmpPass)
+      {
+        return res.status(400).json({error:"Invalid Credentials, try again"})
+      }
+      
+      const data = {
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken=jwt.sign(data, JWT_SECRET)
+      res.json({authtoken});
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Inernal Server Error!")
+    }
+  }
+  )
+  //ROUTE 2: Get logged in user details using POST "api/auth/getuser" LOGIN REQUIRED
+  router.post(
+"/getuser",fetchuser,async(req,res)=>{
+  try {
+    const userId=req.user.id
+    const user =await User.findById(userId).select("-password")
+    res.send(user)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Inernal Server Error!")
+  }
+});
+
+
+
+
 module.exports = router;
