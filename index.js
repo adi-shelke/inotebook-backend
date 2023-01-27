0e0323c0f6fb348a5b3db5a77ac5753d717899d4
Adinath Shelke <shelkeadinath3@gmail.com>
2023-01-27 23:05:43 +0530

    changed port to server port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var cors = require('cors')
 connect()
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
