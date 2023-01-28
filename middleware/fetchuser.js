3327fcd843c62451e1845af816843685a6098430
Adinath Shelke <shelkeadinath3@gmail.com>
2023-01-28 16:59:31 +0530

    fixing the landing page issue

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -5,7 +5,8 @@ const fetchuser = (req,res,next)=>{
     const token = req.header("auth-token");
     if(!token)
     {
-        res.status(401).send({error: "Please authenticate"})
+        // res.status(401).send({error: "Please authenticate"})
+        res.status(401).json({error: "Please authenticate", status: 401})
     }
     try {
         const data = jwt.verify(token,JWT_SECRET)
